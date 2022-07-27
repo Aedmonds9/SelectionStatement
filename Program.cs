208b06b3971d14fc4249b2e7d4a9d2dcfdad1d79
Andrew Edmonds <edmondsandrew9@gmail.com>
2022-07-27 11:39:26 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int favNumber = 7;
+            Console.WriteLine("Hello, try to guess my favorite number!");
+            int userGuess = int.Parse(Console.ReadLine());
+            if (userGuess <= favNumber) ;
+            {
+                Console.WriteLine("Too Low! Try Again!");
+            }
+            else if (userGuess >= favNumber) ;
+            {
+                Console.WriteLine("Too high! Try again!");
+            }
+        }
+    }
+}
